2713574b0d0baae9c9d9148dad5b18f7871daa0c
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-01-29 01:55:33 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,64 @@
+react-init
+============
+Blog con frontend con react y nodejs como backend.
+### Estructura de carpetas
+    .
+    ├── build : CSS y Javascript compilados para navegador (creado al construir)
+    │   ├── css
+    │   └── js : Scripts de /src/app/ compilados
+    │       └── vendor : Librerias externas
+    ├── media
+    └── src
+        ├── app : Scripts principales de cada pagina (jsx)
+        ├── component : Componentes de react (jsx)
+        ├── css
+        └── html
+### JSX
+Un componente de react por archivo en `src/component`. Los componentes se referenciaran entre si utilizando `require` (CommonJS). Ejemplo:
+``` js
+    /*Archivo: src/component/Foo.jsx */
+    
+    // Utilizamos el componente Faa que se encuentra en la misma carpeta
+    var Faa = require("./Faa.jsx");
+    
+    var Foo = React.createClass({
+        render: function(){
+            return(<Faa data="lalala"/>);
+        }
+    });
+    /* Al final de cada componente, hay que almacenarlo en module.exports
+       para que pueda ser utilizado */
+    module.exports = Foo;
+```
+``` js
+    /* Archivo: src/app/index.jsx */
+    
+    // Utilizamos un componente de react en el script
+    var Foo = require("../component/Foo.jsx");
+    
+    React.render(<Foo/>, document.body);
+```
+``` html
+<!-- Archivo: src/html/index.html -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="static/js/vendor/vendor.js" />
+        <script src="/static/js/index.js" />
+    </head>
+    <body></body>
+</html
+```
+### Como utilizar
+Instalar dependencias
+``` bash
+$ npm install
+```
+Para construir.
+``` bash
+$ npm run build
+```
+Para ejecutar
+``` bash
+$ node app.js
+```
---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var del = require('del');
+var reactify = require('reactify');
+var transform = require('vinyl-transform');
+var fs = require('fs');
+var path = require('path');
+var plugins = require('gulp-load-plugins')();
+var gutil = require('gulp-util');
+var buildPosts = require("./buildPosts");
+
+var config = { production: false };
+
+// Scripts
+var appDir = "./src/app/";
+var appPath = appDir + "**/*.jsx";
+var appBuildDir = "./build/js/";
+
+var componentDir = "./src/component/"
+var componentPaths = fs.readdirSync(componentDir);
+var componentBuildDir = "./build/js/";
+
+var vendorDir = "./build/js/";
+var vendorFileName = "vendor.js";
+var vendorModules = [
+/*
+    {
+        bowerName: "showdown",
+        devSource: "src/showdown.js",
+        prodSource: "compressed/showdown.js",
+    },
+*/
+];
+
+// Styles
+var stylesDir ='./src/styles/';
+var stylesBuildDir = './build/css/';
+
+var cssPath = stylesDir + '**/*.css';
+var sassPath = stylesDir + '**/*.scss';
+
+gulp.task('set-production', function(done){
+    config.production = true;
+    gutil.log("Produccion!");
+    done();
+});
+
+gulp.task('clean-css', function(done){
+    del(['build/css'], done);
+});
+gulp.task('clean-js', function(done){
+    del(['build/js'], done);
+})
+
+gulp.task('clean-authors', function(done){
+    del(['build/data/autor'], done);
+})
+
+gulp.task('clean-posts', function(done){
+    del(['build/data/post'], done);
+})
+
+gulp.task('vendor', ['clean-js'], function(){
+    var vendorPathList = vendorModules.map(function(item){
+        var moduleName = config.production?item.prodSource:item.devSource;
+        var modulePath = path.join("bower_components", item.bowerName ,moduleName);
+        return modulePath
+    });
+    return gulp.src(vendorPathList, {base: "bower_components"})
+        .pipe(plugins.concat(vendorFileName))
+        .pipe(config.production ? plugins.uglify() : gutil.noop())
+        .pipe(gulp.dest(vendorDir))
+        .on('error', gutil.log);
+});
+
+gulp.task('browserify', ['clean-js', 'vendor'], function(){
+    //Bundle apps
+    var browserified = transform(function(filename){
+        gutil.log('browserify',filename);
+        var b = browserify(filename);
+        //b.transform(reactify);
+
+        return b.bundle();
+    });
+    return gulp.src(appPath)
+        .pipe(browserified)
+        .pipe(config.production ? plugins.uglify() : gutil.noop())
+        .pipe(plugins.rename(function(filePath){ filePath.extname = ".js";}))
+        .pipe(gulp.dest(appBuildDir));
+});
+
+gulp.task('css', ['clean-css'], function(){
+    return gulp.src(cssPath)
+        .pipe(gulp.dest(stylesBuildDir));
+});
+gulp.task('sass', ['clean-css'], function(){
+    return gulp.src(sassPath)
+        .pipe(plugins.sass())
+        .pipe(gulp.dest(stylesBuildDir));
+});
+
+gulp.task('styles', ['css', 'sass']);
+
+// Produccion
+gulp.task('deploy', ['set-production', 'default']);
+
+// Por defecto, desarrollo
+gulp.task('default', ['vendor', 'browserify', 'styles']);
+
+gulp.task('watch', ['vendor', 'browserify', 'styles'], function(){
+    gulp.watch('src/**/*.jsx', ['browserify']).on('change', function(event){
+        gutil.log(event.type, event.path);
+    });
+    gulp.watch(cssPath, ['css']).on('change', function(event){
+        gutil.log(event.type, event.path);
+    });
+    gulp.watch(sassPath, ['sass']).on('change', function(event){
+        gutil.log(event.type, event.path);
+    });
+});
