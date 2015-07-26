ad338914939c6dcad8987166313e0bade85617be
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-07-25 20:01:55 -0500

    app.js ahora solo sirve la carpeta "build/"

--- app.js
+++ app.js
@@ -7,12 +7,7 @@ var port = process.env.PORT || 8080;
 app.use(compress());
 
 //Serve Static Files
-app.use('/static/media', express.static(__dirname + '/media'));
-app.use('/static', express.static(__dirname + '/build'));
-
-//Serve Html
-app.use('/*', express.static(__dirname + '/src/html'));
-
+app.use('/', express.static(__dirname + '/build'));
 
 //Run server
 app.listen(port);
