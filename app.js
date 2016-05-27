1637e8378c224603995ed0872b9fa8711de8097b
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2016-05-26 20:07:52 -0500

    Dependencias actualizadas y app.js modificado

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 var express = require('express');
-var compress = require('compression');
 var app = express();
 var port = process.env.PORT || 8080;
-
-//Gzip
-app.use(compress());
 
 //Serve Static Files
 app.use('/', express.static(__dirname + '/build'));
